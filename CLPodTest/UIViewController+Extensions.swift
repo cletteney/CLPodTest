2ef5f90e4fdeea67d727a2a6eabe9296605caa06
Chase Letteney <lettene3@uwm.edu>
2021-03-19 16:59:54 -0500

    Added UIViewController+Extensions.swift

---
+++ CLPodTest/UIViewController+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Extensions.swift
+//  CLPodTest
+//
+//  Created by Chase Letteney on 3/19/21.
+//
+import UIKit
+
+extension UIViewController {
+    
+    func testFunc() {
+        print("Testing")
+    }
+    
+}
