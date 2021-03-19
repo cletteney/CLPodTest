38d3fff5e89c352df1a471fd272b6a577687b2de
Chase Letteney <lettene3@uwm.edu>
2021-03-19 16:31:13 -0500

    Initial project setup

---
+++ CLPodTest/CLPodTest.h
@@ -0,0 +1,18 @@
+//
+//  CLPodTest.h
+//  CLPodTest
+//
+//  Created by Chase Letteney on 3/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLPodTest.
+FOUNDATION_EXPORT double CLPodTestVersionNumber;
+
+//! Project version string for CLPodTest.
+FOUNDATION_EXPORT const unsigned char CLPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLPodTest/PublicHeader.h>
+
+
---
+++ CLPodTestExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLPodTestExamples
+//
+//  Created by Chase Letteney on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
